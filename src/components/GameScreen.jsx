93d7c04e0c8b5d9b496db3d5b7f30bb878e90474
aiuyap <186591613+aiuyap@users.noreply.github.com>
2024-12-18 00:32:25 +0800

    feat: added responsive scoreboard

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -1,7 +1,10 @@
 export function GameScreen() {
   return (
     <div>
-      <header className="h-32 bg-black bg-opacity-50 text-white"></header>
+      <header className="flex flex-wrap justify-center gap-16 bg-black bg-opacity-50 py-4 text-2xl font-extrabold text-white sm:gap-20 lg:gap-40">
+        <h1>Score: </h1>
+        <h1>Best Score: </h1>
+      </header>
     </div>
   );
 }
