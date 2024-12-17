9f5dec3c849ac1d1f2c85fb3b290f482845cdfa8
aiuyap <186591613+aiuyap@users.noreply.github.com>
2024-12-18 00:08:57 +0800

    feat: added instruction on start screen

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -1,3 +1,7 @@
 export function GameScreen() {
-  return <div></div>;
+  return (
+    <div>
+      <header className="h-32 bg-black bg-opacity-50 text-white"></header>
+    </div>
+  );
 }
--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,14 +1,23 @@
 export function StartScreen({ onClick }) {
   return (
     <div className="flex h-screen flex-col justify-center bg-black bg-opacity-50 align-middle text-white">
-      <div className="grid">
+      <div className="grid justify-center">
         <h1 className="mb-10 text-center text-6xl font-bold">
           DotA Memory Game
         </h1>
+        <div className="px-4">
+          <h2>Instructions: </h2>
+          <p>
+            - Get points by clicking on a Hero but don&apos;t click it more than
+            once!
+          </p>
+          <p>- Test yourself on how many clicked heroes you can remember.</p>
+          <p>- If you click a hero twice, the score will reset.</p>
+        </div>
         <button
           onClick={onClick}
           type="button"
-          className="h-20 w-40 justify-self-center rounded-3xl bg-red-800 text-lg font-bold transition-colors hover:bg-red-950"
+          className="m-4 h-20 w-40 justify-self-center rounded-3xl bg-red-800 text-lg font-bold transition-colors hover:bg-red-950"
         >
           Start Game
         </button>
