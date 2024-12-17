3cda07e0a4400b49aa5a277ea84e0c3ffdac1959
aiuyap <186591613+aiuyap@users.noreply.github.com>
2024-12-18 00:14:41 +0800

    refactor: adjusted opacity of bg for text clarity

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,12 +1,12 @@
 export function StartScreen({ onClick }) {
   return (
-    <div className="flex h-screen flex-col justify-center bg-black bg-opacity-50 align-middle text-white">
+    <div className="flex h-screen flex-col justify-center bg-black bg-opacity-70 align-middle text-white">
       <div className="grid justify-center">
         <h1 className="mb-10 text-center text-6xl font-bold">
           DotA Memory Game
         </h1>
-        <div className="px-4">
-          <h2>Instructions: </h2>
+        <div className="px-4 text-gray-300">
+          <h2 className="font-bold text-white">Instructions: </h2>
           <p>
             - Get points by clicking on a Hero but don&apos;t click it more than
             once!
