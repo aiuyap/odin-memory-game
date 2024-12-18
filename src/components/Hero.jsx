d7054b01c329b105be6dd07c228cff795b414861
aiuyap <186591613+aiuyap@users.noreply.github.com>
2024-12-18 18:59:55 +0800

    fix: removed hover effect for mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ export function Hero({ heroInfo, onClick }) {
   return (
     <div
       onClick={() => onClick(heroInfo.id)}
-      className="grid max-w-48 transform cursor-pointer rounded-xl bg-black bg-opacity-70 p-4 transition-transform hover:scale-110"
+      className="grid max-w-48 transform cursor-pointer rounded-xl bg-black bg-opacity-70 p-4 transition-transform md:hover:scale-110"
     >
       <img
         className="h-24 w-full"
